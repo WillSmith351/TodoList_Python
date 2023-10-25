b4ba3d0b56822cf4d5f2bcac311a9c92b814d096
WillSmith351 <williamcalvertsmith@gmail.com>
2023-10-25 09:35:46 +0200

    First Commit

---
+++ todolist.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Création de l'interface graphique.
+app = tk.Tk()
+app.title("Notre To-Do List !")
+
+# Configuration de la fenêtre.
+app.geometry('400x300')
+app.mainloop()
