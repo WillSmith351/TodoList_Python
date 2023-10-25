a644466000ff4d8ad01d3dd760a7dfd8b8f13f6d
Chido Le Riz <99027192+Charl2fde@users.noreply.github.com>
2023-10-25 10:42:52 +0200

    ajout des champs du form

--- todolist.py
+++ todolist.py
@@ -9,6 +9,31 @@ app.title('To-Do List')
 # Création du fichier JSON
 fichier_json = 'database.json'
 
+# Ajouter une tâche
+taches_label = tk.Label(app, text="Tâches à faire", font=("Helvetica", 18), fg="blue")
+taches_label.pack()
+taches_entry = tk.Entry(app, font=("Helvetica", 22), width=30)
+taches_entry.pack()
+
+#Ajouter une date d'échéance pour la tâche
+date_label = tk.Label(app, text="Date d'échéance", font=("Helvetica", 18), fg="blue")
+date_label.pack()
+date_entry = tk.Entry(app, font=("Helvetica", 22), width=30)
+date_entry.pack()
+
+
+# Boutton valider
+valider_button = tk.Button(app, text="Valider", font=("Helvetica", 16), bg="green", fg="white", width=13)
+valider_button.pack()
+
+# Boutton supprimer
+supprimer_button = tk.Button(app, text="Supprimer", font=("Helvetica", 16), bg="red", fg="white", width=13)
+supprimer_button.pack()
+
+# Boutton Terminer
+terminer_button = tk.Button(app, text="Tâche terminée", font=("Helvetica", 16), bg="white", fg="black", width=13)
+terminer_button.pack()
+
 # Suppression de toutes les données de l'utilisateur dans le fichier JSON.
 def delete_all_data(choice):
     if choice == 'true':
