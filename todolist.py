91cf9f6a2f8c9e04cd4f0ea6a671911f1b11911f
WillSmith351 <williamcalvertsmith@gmail.com>
2023-10-25 12:31:31 +0200

    update

--- todolist.py
+++ todolist.py
@@ -21,7 +21,7 @@ def enregistrer_donnees():
     with open(fichier_json, 'r') as fichier:
         data = json.load(fichier)
     with open(fichier_json, 'w') as fichier:
-        data[tache] = date
+        data[tache] = {'date': date, 'status': 'en cours'} 
         json.dump(data, fichier)
     messagebox.showinfo("Succès", "Données enregistrées avec succès !")
     afficher_taches()
@@ -31,9 +31,7 @@ def enregistrer_donnees():
 def afficher_taches():
     with open(fichier_json, 'r') as fichier:
         data = json.load(fichier)
-        taches_combobox['values'] = [f"Tâche : {tache} - Date : {date}" for tache, date in data.items()]
-
-
+        taches_combobox['values'] = [f"Tâche : {tache} - Date : {data[tache]['date']} - Statut : {data[tache]['status']}" for tache in data]
 
 
 
@@ -42,16 +40,21 @@ taches_label = tk.Label(app, text="Tâches à faire", font=("Helvetica", 18), fg
 taches_label.pack()
 taches_entry = tk.Entry(app, font=("Helvetica", 22), width=30)
 taches_entry.pack()
+
 date_label = tk.Label(app, text="Date d'échéance (jour/mois/année)", font=("Helvetica", 18), fg="blue")
 date_label.pack()
 date_entry = tk.Entry(app, font=("Helvetica", 22), width=30)
 date_entry.pack()
+
 valider_button = tk.Button(app, text="Valider", font=("Helvetica", 16), bg="green", fg="white", width=13, command=enregistrer_donnees)
 valider_button.pack()
+
 supprimer_button = tk.Button(app, text="Supprimer", font=("Helvetica", 16), bg="red", fg="white", width=13)
 supprimer_button.pack()
+
 terminer_button = tk.Button(app, text="Tâche terminée", font=("Helvetica", 16), bg="white", fg="black", width=13)
 terminer_button.pack()
+
 taches_combobox = ttk.Combobox(app, font=("Helvetica", 14), width=50)
 taches_combobox.pack()
 
